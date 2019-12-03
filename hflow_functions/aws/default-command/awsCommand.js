daddf547dcf7520bb7646dac9eb98c60d477d6e6
Paul Banach <pawel.banach@chargify.com>
2019-12-03 18:40:15 +0100

    Update aws command

--- hflow_functions/aws/default-command/awsCommand.js
+++ hflow_functions/aws/default-command/awsCommand.js
@@ -4,57 +4,60 @@ const executorConfig = require('./awsCommand.config.js');
 const identity = function (e) {return e};
 
 function retryStrategy(err, response, body) {
-    return err || response.statusCode >= 400 || request.RetryStrategies.HTTPOrNetworkError(err, response);
+  return err || response.statusCode >= 400 || request.RetryStrategies.HTTPOrNetworkError(err, response);
 }
 
 function awsCommand(ins, outs, config, cb) {
 
-    let options = executorConfig.options;
-    if (config.executor.hasOwnProperty('options')) {
-        let executorOptions = config.executor.options;
-        for (let opt in executorOptions) {
-            if (executorOptions.hasOwnProperty(opt)) {
-                options[opt] = executorOptions[opt];
-            }
-        }
+  const options = executorConfig.options;
+  if (config.executor.hasOwnProperty('options')) {
+    const executorOptions = config.executor.options;
+    for (let opt in executorOptions) {
+      if (executorOptions.hasOwnProperty(opt)) {
+        options[opt] = executorOptions[opt];
+      }
     }
-    let executable = config.executor.executable;
-    let jobMessage = {
-        "executable": executable,
-        "args": config.executor.args,
-        "env": (config.executor.env || {}),
-        "inputs": ins.map(identity),
-        "outputs": outs.map(identity),
-        "options": options
-    };
+  }
+  const executable = config.executor.executable;
+  const jobMessage = {
+    "executable": executable,
+    "args": config.executor.args,
+    "env": (config.executor.env || {}),
+    "inputs": ins.map(identity),
+    "outputs": outs.map(identity),
+    "options": options
+  };
 
-    console.log("Executing:  " + JSON.stringify(jobMessage));
+  console.log("Executing:  " + JSON.stringify(jobMessage));
 
-    let functionType = config.deploymentType ? config.deploymentType : executorConfig.functionType;
-    let url = executorConfig.resources[functionType];
+  const functionType = config.deploymentType ? config.deploymentType : executorConfig.functionType;
+  const url = executorConfig.resources[functionType];
 
-    function optionalCallback(err, response, body) {
-        if (err) {
-            console.log("Function: " + executable + " error: " + err);
-            cb(err, outs);
-            return
-        }
-        if (response) {
-            console.log("Function: " + executable + " response status code: " + response.statusCode + " number of request attempts: " + response.attempts)
-        }
-        console.log("Function: " + executable + " id: " + config.id + " resource: " + functionType + " data: " + body.message);
-        cb(null, outs);
+  function optionalCallback(err, response, body) {
+    const request_end = Date.now();
+    if (err) {
+      console.log("Function: " + executable + " error: " + err);
+      cb(err, outs);
+      return
     }
+    if (response) {
+      console.log("Function: " + executable + " response status code: " + response.statusCode + " number of request attempts: " + response.attempts)
+    }
+    const request_duration = request_end - request_start;
+    console.log("Function: " + executable + " id: " + config.id + " resource: " + functionType + " request_start: " + request_start + " request_end: " + request_end + " request_duration: " + request_duration + " data: " + body.message);
+    cb(null, outs);
+  }
 
-    request.post({
-        retryDelay: 1000,
-        timeout: 600000,
-        retryStrategy: retryStrategy,
-        maxAttempts: 30,
-        url: url,
-        json: jobMessage,
-        headers: {'Content-Type': 'application/json', 'Accept': '*/*'}
-    }, optionalCallback);
+  const request_start = Date.now();
+  request.post({
+    retryDelay: 1000,
+    timeout: 600000,
+    retryStrategy: retryStrategy,
+    maxAttempts: 30,
+    url: url,
+    json: jobMessage,
+    headers: {'Content-Type': 'application/json', 'Accept': '*/*'}
+  }, optionalCallback);
 
 }
 
